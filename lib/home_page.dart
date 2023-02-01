7a6e3a9c2fc9d2e6186eda0581ba059bd945c00e
Niko <cabraln1@uci.edu>
2023-02-01 14:48:47 -0800

    2/1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +9,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('signed in'),
+      body: Center (
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('signed in as: cabralniko@gmail.com'),
+          MaterialButton(
+            onPressed:() {
+              FirebaseAuth.instance.signOut();
+    },
+    color: Colors.deepOrangeAccent[200],
+    child: Text('sign out'),
+    )
+    ],
+    ),
+      ),
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:fellowship/home_page.dart';
+import 'package:fellowship/login_page.dart';
+import 'package:fellowship/main_page.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-// import 'package:firebasetutorial/login_page.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:MainPage(),
+      home: MainPage(),
     );
   }
 }
